01e4dc1537aa39cf280157cf95a8b8976cdd4854
Gavin Xue <gavinxue050705@gmail.com>
2023-12-19 11:26:11 -0500

    Deleted tablinks

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
 
-// tab-links
-var tablinks = document.getElementsByClassName('tab-links')
-var tabcontents = document.getElementsByClassName("tab-contents")
-
-function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
 // sidemenu
 var sidemenu = document.getElementById("sidemenu");
 
